db6dcff2481040880783fe1c49c2143fc6b06772
Aasishm <46561984+Aasishm@users.noreply.github.com>
2019-10-07 13:12:32 +0530

    Add files via upload
    Solution to the Electronics Shop problem

---
+++ Electronics Shop.py
@@ -0,0 +1,18 @@
+import os 
+import sys
+bnm=list(map(int,input().rstrip().split()))
+b=int(bnm[0])
+n=int(bnm[1])
+m=int(bnm[2])
+k=list(map(int,input().rstrip().split()))
+d=list(map(int,input().rstrip().split()))
+s=[]
+if min(k)+min(d)>b:
+	print('-1')
+elif min(k)+min(d)<=b:
+	for i in k:
+		for j in d:
+			sum=i+j
+			if sum<=b:
+				s.append(sum)
+	print(max(s))
